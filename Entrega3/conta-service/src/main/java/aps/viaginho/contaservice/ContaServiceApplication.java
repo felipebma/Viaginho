81453cade83cbbf2c0f7aa10178126c1b6edd6fa
felipebma <fbma@cin.ufpe.br>
2021-12-12 03:58:16 -0300

    Create Conta-service

---
+++ Entrega3/conta-service/src/main/java/aps/viaginho/contaservice/ContaServiceApplication.java
@@ -0,0 +1,13 @@
+package aps.viaginho.contaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaServiceApplication.class, args);
+	}
+
+}
---
+++ Entrega3/conta-service/src/test/java/aps/viaginho/contaservice/ContaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.viaginho.contaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
