aadde6831ffc7674518317f9e269cf1c32484e93
felipebma <fbma@cin.ufpe.br>
2021-12-13 10:39:58 -0300

    Create client-frontend

---
+++ Entrega3/cliente-frontend/src/main/java/aps/viaginho/clientefrontend/ClienteFrontendApplication.java
@@ -0,0 +1,13 @@
+package aps.viaginho.clientefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteFrontendApplication.class, args);
+	}
+
+}
---
+++ Entrega3/cliente-frontend/src/test/java/aps/viaginho/clientefrontend/ClienteFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.viaginho.clientefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
