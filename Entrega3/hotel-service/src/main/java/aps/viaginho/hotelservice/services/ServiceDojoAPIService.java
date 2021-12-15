c99bfa8b02fef4975642cd2ca4e8c488c34795f2
felipebma <fbma@cin.ufpe.br>
2021-12-15 05:51:05 -0300

    Dockerize microsservices

--- Entrega3/hotel-service/src/main/java/aps/viaginho/hotelservice/services/ServiceDojoAPIService.java
+++ Entrega3/hotel-service/src/main/java/aps/viaginho/hotelservice/services/ServiceDojoAPIService.java
@@ -3,6 +3,7 @@ package aps.viaginho.hotelservice.services;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,8 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 import aps.viaginho.hotelservice.model.CitySearchResponse;
 import aps.viaginho.hotelservice.model.HotelSearchData;
-import aps.viaginho.hotelservice.model.RapidAPIHotelResponse;
 import aps.viaginho.hotelservice.model.HotelSearchResponse.Hotel;
+import aps.viaginho.hotelservice.model.RapidAPIHotelResponse;
 import aps.viaginho.hotelservice.model.RapidAPIHotelResponse.Result;
 
 @Service
@@ -35,7 +36,8 @@ public class ServiceDojoAPIService implements HotelAPIServiceInterface {
         headers.set("x-rapidapi-key", apiKey);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<CitySearchResponse> response = restTemplate.exchange(String.format(url, hotelSearchData.getCity()), HttpMethod.GET, entity,
+        ResponseEntity<CitySearchResponse> response = restTemplate.exchange(
+                String.format(url, hotelSearchData.getCity()), HttpMethod.GET, entity,
                 CitySearchResponse.class);
 
         return response.getBody().getSuggestions().get(0).getEntities().get(0).getDestinationId();
@@ -55,14 +57,17 @@ public class ServiceDojoAPIService implements HotelAPIServiceInterface {
         headers.set("x-rapidapi-key", apiKey);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<RapidAPIHotelResponse> response = restTemplate.exchange(String.format(url, cityId, hotelSearchData.getStartDate(), hotelSearchData.getEndDate()), HttpMethod.GET, entity,
-        RapidAPIHotelResponse.class);
+        ResponseEntity<RapidAPIHotelResponse> response = restTemplate.exchange(
+                String.format(url, cityId, hotelSearchData.getStartDate(), hotelSearchData.getEndDate()),
+                HttpMethod.GET, entity,
+                RapidAPIHotelResponse.class);
 
         List<Hotel> hotels = new ArrayList<Hotel>();
 
-        for(int i = 0; i < response.getBody().getData().getBody().getSearchResults().getResults().size(); i++) {
+        for (int i = 0; i < response.getBody().getData().getBody().getSearchResults().getResults().size(); i++) {
             Result result = response.getBody().getData().getBody().getSearchResults().getResults().get(i);
-            if(result.getRatePlan() == null) continue;
+            if (result.getRatePlan() == null)
+                continue;
 
             Hotel hotel = new Hotel();
             hotel.setName(result.getName());
